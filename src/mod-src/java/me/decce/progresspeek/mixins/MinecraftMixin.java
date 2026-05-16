b60086c2085439c418049e6a03bb907093dfd756
decce6 <decce6@proton.me>
2026-05-16 16:07:58 +0800

    Add disconnect progress

--- src/mod-src/java/me/decce/progresspeek/mixins/MinecraftMixin.java
+++ src/mod-src/java/me/decce/progresspeek/mixins/MinecraftMixin.java
@@ -1,8 +1,12 @@
 package me.decce.progresspeek.mixins;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import me.decce.transformingbase.core.ProgressPeekCore;
+import me.decce.transformingbase.core.ProgressStatus;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Overlay;
+import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,5 +19,12 @@ public class MinecraftMixin {
         if (overlay == null) {
             ProgressPeekCore.transitionToNoProgress();
         }
+    }
+
+    @WrapMethod(method = "disconnect(Lnet/minecraft/client/gui/screens/Screen;ZZ)V")
+    public void progresspeek$disconnect(Screen screen, boolean keepResourcePacks, boolean stopSound, Operation<Void> original) {
+        ProgressPeekCore.setStatus(ProgressStatus.INDETERMINATE);
+        original.call(screen, keepResourcePacks, stopSound);
+        ProgressPeekCore.transitionToNoProgress();
     }
 }
