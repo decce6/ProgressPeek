7bfb985847cfc40102f8012a6c70a7f01e4b9a02
decce6 <decce6@proton.me>
2026-05-17 16:47:15 +0800

    Fix mod name

--- core/src/main/java/me/decce/transformingbase/constants/Constants.java
+++ core/src/main/java/me/decce/transformingbase/constants/Constants.java
@@ -2,7 +2,7 @@ package me.decce.transformingbase.constants;
 
 public class Constants {
     public static final String MOD_ID = "progresspeek";
-    public static final String MOD_NAME = "Example Mod";
+    public static final String MOD_NAME = "Progress Peek";
     public static final String CORE_PACKAGE = "me.decce." + MOD_ID + ".core";
     public static final String FORGE_JIJ_NAME = MOD_ID + "-forge-mod.jar";
     public static final String NATIVE_METHOD_PREFIX = MOD_ID + "_wrapped$";
