2c6384ee4cf8346d117424c72acb3b896bec745f
decce6 <decce6@proton.me>
2026-05-26 11:57:18 +0800

    Add progress when opening world (preparing resources)

---
+++ src/mod-src/java/me/decce/progresspeek/mixins/WorldOpenFlowsMixin.java
@@ -0,0 +1,24 @@
+package me.decce.progresspeek.mixins;
+
+import com.mojang.serialization.Dynamic;
+import me.decce.transformingbase.core.ProgressPeekCore;
+import me.decce.transformingbase.core.ProgressStatus;
+import net.minecraft.client.gui.screens.worldselection.WorldOpenFlows;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldOpenFlows.class)
+public class WorldOpenFlowsMixin {
+    @Inject(method = "openWorldLoadLevelStem", at = @At("HEAD"))
+    private void progresspeek$openWorldLoadLevelStem(CallbackInfo ci) {
+        ProgressPeekCore.setStatus(ProgressStatus.INDETERMINATE);
+    }
+
+    @Inject(method = "openWorldLoadLevelStem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V"))
+    private void progresspeek$openWorldLoadLevelStem$1(CallbackInfo ci) {
+        ProgressPeekCore.transitionToNoProgress(false);
+    }
+}
